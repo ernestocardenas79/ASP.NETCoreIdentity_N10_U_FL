ad68615276eaa2701e07cb68d40bc2a9108aeae6
ernesto.cardenas <ernesto.cardenas@REM-LT-250811C>
2026-04-27 13:58:27 -0700

    Add project files.

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential {  get; set; }  = new Credential();
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if(Credential.UserName == "admin" && Credential.Password == "admin")
+            {
+                //Crating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, Credential.UserName),
+                    new Claim(ClaimTypes.Email,"admin@thisNewSite")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ WebApp_UnderTheHood/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
