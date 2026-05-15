b5fc98d4609732e9f64630e1cd222d4b22569be4
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2026-05-15 12:11:58 -0700

    Add Authorization to the WeatherForecast endpoint, that includes adding JWT Bearer validations.

--- Web.API/Program.cs
+++ Web.API/Program.cs
@@ -1,14 +1,36 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+
+var secretKey = builder.Configuration["SecretKey"];
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey ?? string.Empty)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
